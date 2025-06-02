39b60f3f73a50861880d0f1843238d1d3e414ce5
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-02 09:41:11 -0400

    Time: 1 ms (77.20%) | Memory: 18.9 MB (63.26%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -12,9 +12,7 @@ class Solution:
     
     def balanceHeight(self, root: Optional[TreeNode]) :
         if root is None : return [True, 0]
-        left = self.balanceHeight(root.left)
-        right = self.balanceHeight(root.right)
-        isBalanced = left[0] and right[0] and abs(left[1]-right[1]) <= 1
-        return  [isBalanced, 1+max(left[1], right[1]) ]
+        left, right = self.balanceHeight(root.left), self.balanceHeight(root.right)
+        return  [(left[0] and right[0] and abs(left[1]-right[1]) <= 1) , 1+max(left[1], right[1]) ]
     
     
