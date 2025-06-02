7be97c1de2e452faf3b3701157a5253722ef01a1
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-02 08:53:46 -0400

    Time: 4 ms (74.04%) | Memory: 20.9 MB (48.96%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,18 +6,24 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        #Create a global variable in which we store the current max diameter
         self.maxDiam = 0
         if root is None:
             return 0
         else:
+            #Call the diameter helper recursively, the maxDiam will get updated automatically -
+            #if needed, then returned
             self.diameterOfBinaryTreeHelper(root)
             return self.maxDiam
     
     def diameterOfBinaryTreeHelper(self, root) -> int:
+        #If we are past a leaf node, stop recurring
         if root is None: 
             return 0
         else: 
+            #Find the depths of the subtree to the left and to the right
             leftDepth = self.diameterOfBinaryTreeHelper(root.left)
             rightDepth = self.diameterOfBinaryTreeHelper(root.right)
+            #Compare if at the given root the diameter is bigger than the current max
             self.maxDiam = max(leftDepth+rightDepth, self.maxDiam)
             return 1 + max(leftDepth, rightDepth)
