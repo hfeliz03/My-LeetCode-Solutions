a3a6f073d5f834ded242af7a1292898e565b2031
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-04 10:05:41 -0400

    Time: 56 ms (39.63%) | Memory: 34.2 MB (68.08%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -5,14 +5,15 @@ class Solution:
         if len(nums) == 1: return 1
     
 
-        i, totalCount, count = 0, 1 , 1 
+        i, totalCount = 0, 1 
         for num in numSet:
             if num - 1 not in numSet:
+                count = 1
                 j = 1
                 while num+j in numSet:
                     count+=1
                     j+=1
-            totalCount = max(totalCount, count)
-            count = 1
+                totalCount = max(totalCount, count)
+            
 
         return totalCount
