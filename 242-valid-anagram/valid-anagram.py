8add02020dda000627310b364f3557afee07afeb
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-06 10:44:43 -0400

    Time: 25 ms (5.27%) | Memory: 17.9 MB (49.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,12 +2,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):return False
         freq = {}
-        for i in s:
-            if i not in freq.keys(): freq[i] = 1
-            else: freq[i] += 1
-        for i in t:
-            if i not in freq.keys(): return False
-            else: freq[i] -= 1
+        i = 0
+        while i < len(s):
+            if s[i] not in freq.keys(): freq[s[i]] = 1
+            else: freq[s[i]] += 1
+            if t[i] not in freq.keys(): freq[t[i]] = -1
+            else: freq[t[i]] -= 1
+            i +=1
         for i in freq.keys():
             if freq[i] != 0: return False
         return True
