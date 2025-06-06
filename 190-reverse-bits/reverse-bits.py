0270a1008239d024487a73d874bdfec4e54f71de
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-06 10:28:47 -0400

    Time: 36 ms (75.19%) | Memory: 17.7 MB (56.44%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,9 +1,5 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         binaryString = bin(n)[2:].zfill(32)
-        reversedBinaryString =""
-        i = -1
-        while -i <= len(binaryString):
-            reversedBinaryString += binaryString[i]
-            i -=1
+        reversedBinaryString = binaryString[::-1] 
         return int(reversedBinaryString, 2)
