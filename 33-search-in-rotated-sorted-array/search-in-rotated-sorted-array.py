58b2c9c9b462eed22d68deb77386f2456d718e29
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-10 09:17:29 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (66.40%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,28 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if not nums: return -1
-        l , r = 0, len(nums)-1
-        if nums[l] == target: return 0
-        if nums[r] == target: return r
-        
+        l, r = 0, len(nums) - 1
+
+        # Find the index of the smallest element 
         while l < r:
-            mid = l+(r-l)//2 
+            mid = l + (r - l) // 2
             if nums[mid] > nums[r]:
-                l = mid+1
+                l = mid + 1
             else:
                 r = mid
-        k = l
-    
-        
-        l , r = 0, (len(nums)-1)
-        
-        while l <= r:
-            mid = (l+r)//2
-            real_mid = (mid+k) % len(nums)
-            if nums[real_mid] == target: return real_mid
-            if  nums[real_mid] > target:
-                r = mid-1
-            else:
-                l = mid+1
-        return -1
+        k = l  # pivot index
 
+        # Standard binary search with adjusted indices
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            real_mid = (mid + k) % len(nums)
+            if nums[real_mid] == target:
+                return real_mid
+            elif nums[real_mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return -1
