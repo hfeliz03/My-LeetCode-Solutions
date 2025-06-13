59a184125a58fd7cdd18c48d151b5237ee8824d1
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-13 08:53:10 -0400

    Time: 10 ms (12.34%) | Memory: 17.9 MB (35.99%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        curr1, curr2 = l1, l2
+        dummy = ListNode()
+        dummyCurr = dummy
+        carryOne = False
+        while curr1 and curr2:
+            sumVal = (curr1.val + curr2.val)
+            if carryOne == True:sumVal +=1
+            ListNode((sumVal + 1)%10)
+            dummyCurr.next = ListNode(sumVal%10)
+            carryOne = True if sumVal >= 10 else False
+            dummyCurr = dummyCurr.next
+            curr1, curr2 = curr1.next, curr2.next
+        while curr1:
+            val = curr1.val
+            if carryOne == True: val+=1
+            carryOne = True if val >= 10 else False
+            dummyCurr.next = ListNode(val%10)
+            dummyCurr = dummyCurr.next
+            curr1 = curr1.next
+            print(val)
+        while curr2:
+            val = curr2.val
+            if carryOne == True: val+=1
+            carryOne = True if val >= 10 else False
+            dummyCurr.next = ListNode(val%10)
+            dummyCurr = dummyCurr.next
+            curr2 = curr2.next
+        if carryOne == True: dummyCurr.next = ListNode(1)
+        return dummy.next
