8705c1ba44b2a8979c6a30b2fd1a110193feacba
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-20 16:10:39 -0400

    Time: 3 ms (65.69%) | Memory: 19.5 MB (77.76%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -3,6 +3,5 @@ class Solution:
         if not nums: return None
         if len(nums) == 1: return nums[0]
         carry = 0
-        for num in nums:
-            carry = carry ^ num
+        for num in nums: carry = carry ^ num
         return carry
