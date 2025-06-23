3236b00334aff2e78ff2c5aac6cb31aaa1ce2cfd
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-23 09:38:41 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.06%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -6,21 +6,20 @@ class Solution:
         
         while l < r and t < b:
             #Cover top row:
-            for i in range(l, r):
-                spiralMtx.append(matrix[t][i])
+            for i in range(l, r): spiralMtx.append(matrix[t][i])
             t += 1
-
-            for i in range(t, b):
-                spiralMtx.append(matrix[i][r-1])
+            
+            #Cover right row
+            for i in range(t, b): spiralMtx.append(matrix[i][r-1])
             r -= 1
 
             if not(l<r and t<b): break
-        
-            for i in range(r - 1, l - 1, -1):
-                spiralMtx.append(matrix[b-1][i])
+
+            #Cover bottom row
+            for i in range(r - 1, l - 1, -1): spiralMtx.append(matrix[b-1][i])
             b-=1
 
-            for i in range(b-1, t-1, -1):
-                spiralMtx.append(matrix[i][l])
+            #Cover left col
+            for i in range(b-1, t-1, -1): spiralMtx.append(matrix[i][l])
             l += 1
         return spiralMtx
