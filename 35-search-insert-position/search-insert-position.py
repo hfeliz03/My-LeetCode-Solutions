c551b9c93b48dad42296efeb7c594f3188443875
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-06-26 08:38:59 -0400

    Time: 0 ms (100.00%) | Memory: 18.6 MB (25.42%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,12 +1,11 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if not nums: return 0
-        l,r = 0, len(nums)-1
-        m = len(nums)//2
+        l,r, m = 0, len(nums)-1, len(nums)//2
         while l <= r:
-            if nums[m]==target: return m
+            if nums[m] == target: return m
             if nums[m] > target: r = m-1
             else: l = m+1
             m = (r+l)//2
-        return m+1
+        return m+1 #It's easy to see why we are returning m, the +1 is given to the fact that if not found it shall be placed one space after than m
         
