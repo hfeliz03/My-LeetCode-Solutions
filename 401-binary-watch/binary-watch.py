dac1ce44b3a3224f848ef4d06c343a93156dc0e4
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2025-07-01 09:09:28 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (5.51%) - LeetSync

--- 401-binary-watch/binary-watch.py
+++ 401-binary-watch/binary-watch.py
@@ -3,8 +3,8 @@ class Solution:
         if turnedOn>8 : return []
         
         res = []
-        for h in range(12):
-            for m in range(60):
+        for h in range(12): #hours from [0,11] max 3 1's
+            for m in range(60): #minutes from [0,59] max 5 1's
                 if bin(h).count('1') + bin(m).count('1') == turnedOn: 
                     res.append(f"{h}:{m:02d}")
         return res
