4b181b8eed154b2cf9a94566eb2e2b5ea23c0f89
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-01-02 15:26:12 -0500

    Time: 0 ms (100%), Space: 17.6 MB (86.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,33 +1,34 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        m, n = len(nums1), len(nums2)
-        i, j, o = 0, 0, 0
+        m = len(nums1)
+        n = len(nums2)
+        o = 0
+        i,j = 0, 0
         curr = []
-
-        def shiftPointer(numArray):
-            nonlocal i, j, o
-            if numArray == 1:
+        while i < m and j < n:
+            if nums1[i] < nums2[j]:
                 curr.append(nums1[i])
                 i+=1
             else:
                 curr.append(nums2[j])
                 j+=1
-            o += 1
-
-        while i < m and j < n:
-            if nums1[i] < nums2[j]:
-                shiftPointer(1)
-            else:
-                shiftPointer(2)
-
+            o+=1 
+            # print(curr)
+            # print(o)
             if o > (m+n)/2: 
                 return (curr[-1] + curr[-2])/2 if (m+n) % 2 == 0 else curr[-1]
 
         while i < m and o <= (m+n)/2:
-            shiftPointer(1)
+            curr.append(nums1[i])
+            i+=1
+            o+=1 
 
         while j < n and o <= (m+n)/2:
-            shiftPointer(2)
+            curr.append(nums2[j])
+            j+=1
+            o+=1 
+            print(curr)
+            print(o)
 
         return (curr[-1] + curr[-2])/2 if (m+n) % 2 == 0 else curr[-1]
         
