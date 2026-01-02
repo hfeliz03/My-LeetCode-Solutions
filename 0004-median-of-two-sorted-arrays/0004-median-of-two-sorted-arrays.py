083c63e77044fc6d74baa2acdf81125805f29700
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-01-02 15:19:08 -0500

    Time: 5 ms (22.5%), Space: 17.6 MB (86.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,9 +1,10 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        m, n = len(nums1), len(nums2)
-        i, j, o = 0, 0, 0
+        m = len(nums1)
+        n = len(nums2)
+        o = 0
+        i,j, o = 0, 0, 0
         curr = []
-
         while i < m and j < n:
             if nums1[i] < nums2[j]:
                 curr.append(nums1[i])
@@ -12,7 +13,10 @@ class Solution:
                 curr.append(nums2[j])
                 j+=1
             o+=1 
-            if o > (m+n)/2: return (curr[-1] + curr[-2])/2 if (m+n) % 2 == 0 else curr[-1]
+            # print(curr)
+            # print(o)
+            if o > (m+n)/2: 
+                return (curr[-1] + curr[-2])/2 if (m+n) % 2 == 0 else curr[-1]
 
         while i < m and o <= (m+n)/2:
             curr.append(nums1[i])
@@ -23,6 +27,8 @@ class Solution:
             curr.append(nums2[j])
             j+=1
             o+=1 
+            print(curr)
+            print(o)
 
         return (curr[-1] + curr[-2])/2 if (m+n) % 2 == 0 else curr[-1]
         
