de2479b714a401378bf0e991199db82a1ba2c647
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-01-19 20:03:28 -0500

    Time: 167 ms (5.15%) | Memory: 19.4 MB (15.44%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -3,9 +3,11 @@ class Solution:
         n = len(nums)
         ans = [-1] * n    
         for i in range(n):
+            curNum = nums[i]
+            if curNum % 2 == 0: continue
             j = 0
-            while j < nums[i]:
-                if (j | (j+1)) == nums[i]:
+            while j < curNum:
+                if (j | (j+1)) == curNum:
                     ans[i] = j
                     break
                 j+=1
