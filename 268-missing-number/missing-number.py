1573eee8a06324b00d330bae71fed6ca4a9ed939
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-01-20 13:32:11 -0500

    Time: 3 ms (62.55%) | Memory: 21.5 MB (5.04%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        setNums = set(range(n+1))
-        print(setNums)
-        for i in nums:
-            setNums.discard(i)
-
-        return setNums.pop()
+        setNumsMiss = set(nums)
+        fullSetNums = set(range(len(nums)+1))
+        return (fullSetNums - setNumsMiss).pop()
