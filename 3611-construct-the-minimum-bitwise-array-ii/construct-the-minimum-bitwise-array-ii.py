4668f5374fb15171bda27472c4c62095cafae369
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-01-21 11:56:38 -0500

    Time: 54 ms (5.66%) | Memory: 19.6 MB (13.21%) - LeetSync

--- 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.py
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.py
@@ -1,24 +1,24 @@
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [-1]*n 
-        for x, num in enumerate(nums):
-            if num % 2 == 0: continue
+        ans = []
+        for num in nums:
+            if num % 2 == 0: 
+                ans.append(-1)
+                continue
+
             numBi = bin(num)[2:]
         
             for i in range(len(numBi)):
-                if numBi[i] == "0": continue
+                if numBi[i] == "0": 
+                    continue
+
                 curBinaryStr = list(numBi)
                 curBinaryStr[i] = "0" 
                 curBinaryStr = "".join(curBinaryStr)
                 curBinaryInt = int(curBinaryStr, 2)
-                print(curBinaryInt)
-                print(curBinaryInt + 1)
-                print(curBinaryInt | (curBinaryInt + 1))
-                print("HHH")
+
                 if (curBinaryInt|(curBinaryInt + 1)) == num:
-                    
-                    ans[x] = curBinaryInt
+                    ans.append(curBinaryInt)
                     break
         
         return ans
