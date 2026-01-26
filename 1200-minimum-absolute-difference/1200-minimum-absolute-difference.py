483bc11df0b9cf5e21a3036140c27848c96d463b
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-01-25 21:56:09 -0500

    Time: 51 ms (65.99%), Space: 31.4 MB (38.49%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -6,13 +6,11 @@ class Solution:
 
         for i in range(len(arr)-1):
             a, b = arr[i], arr[i+1] 
-            dif = abs(b-a)
+            dif = b-a
             if dif < minAbsDif:
                 minAbsDif = dif
                 pairs = [[a,b]]
             elif dif == minAbsDif:
                 pairs.append([a,b])
-            else:
-                continue
         
         return pairs
