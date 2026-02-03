db59185e35cfc8024921a43a2b06316026830c2c
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-02-03 10:38:30 -0500

    Time: 3 ms (87.93%) | Memory: 19.5 MB (19.11%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.py
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -4,15 +4,11 @@ class Solution:
         for i in range(1, len(nums)):
             if (nums[i] < nums[i-1]) and (p == -1 )and (i-1 != 0):
                 p = i-1
-                print("1")
             elif nums[i] > nums[i-1] and p != -1 and q == -1:
                 q = i
-                print("2")
             elif nums[i] > nums[i-1] and ((p == -1 and q == -1) or (p != -1 and q != -1)):
-                print("3")
                 continue
             elif nums[i] < nums[i-1] and p != -1 and q == -1:
-                print("4")
                 continue
             else:
                 return False
