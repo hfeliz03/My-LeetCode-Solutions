5d610791b80bc940731b83532d371f09cefbe72c
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-02-05 11:32:32 -0500

    Time: 70 ms (7.17%), Space: 19.5 MB (10.76%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -1,4 +1,3 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        return [nums[(i + nums[i]) % n] for i in range(n)]
+        return [nums[(i + nums[i]) % len(nums)] for i in range(len(nums))]
