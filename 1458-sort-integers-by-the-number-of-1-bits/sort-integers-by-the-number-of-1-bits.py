e716e6ae91ab414c8bb839fca0074e44dabe4988
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-02-24 23:50:50 -0500

    Time: 3 ms (87.88%) | Memory: 19.8 MB (7.90%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.py
@@ -2,17 +2,16 @@ class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
         res = []
         resDict = {}
+        arr.sort()
         for num in arr:
             num1s = num.bit_count()
             atNum1s = resDict.get(num1s, [])
-            atNum1s.append(bin(num)[2:])
-            resDict[num1s] = atNum1s
+            atNum1s.append(num)
+            resDict[num1s] = atNum1s #We know they got the same 
 
         for setOfSame1s in sorted(resDict.keys()):
-            currArr = resDict[setOfSame1s]
-            for i in range(len(currArr)):
-                currArr[i] = int(currArr[i],2)
-            res.append(sorted(currArr))
+            currArr = resDict[setOfSame1s] #Current array of the numbers with same number of 1
+            res.append(currArr)
 
         print(res)
         return [number for sublist in res for number in sublist]
