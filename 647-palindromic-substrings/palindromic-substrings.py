34499db54b89c5fcfb23e9f39eed69260ad73f90
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-01 17:10:37 -0500

    Time: 115 ms (79.07%) | Memory: 19.1 MB (90.76%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,31 +1,51 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = [1] * len(s) #Each individual char in s is a palindromic substring by itself
-        # I know how to check if a substring is palindromic in n/2 time
-        # Check if each substr is palindromic and add it to a set in case theres later repetitions
-        #Check in n^2 if all substrs are palindromic. increase their index at res
 
-        def isPalin(substr) -> bool:
-            l, r = 0, len(substr) - 1
-            while l < r:
-                if substr[l] != substr[r]:
-                    return False
-                l+=1
-                r-=1
+        res = 0
+        i = 0
+        while i < len(s):
+            l = r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
 
-            return True
-            
-        palindromes = set()
-        for l in range(len(s)):
-            for r in range(l+1, len(s)):
-                substr = s[l:r+1]
-                if substr in palindromes:
-                    res[l] += 1
-                elif isPalin(substr) : 
-                    palindromes.add(substr)
-                    res[l] += 1
-                else:
-                    continue
+            l = i
+            r = i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+            i += 1
+        return res
+
+        # res = [1] * len(s) 
+        # #Each individual char in s is a palindromic substring by itself
+        # # I know how to check if a substring is palindromic in n/2 time
+        # # Check if each substr is palindromic and add it to a set in case theres later repetitions
+        # #Check in n^2 if all substrs are palindromic. increase their index at res
+
+        # def isPalin(substr) -> bool:
+        #     l, r = 0, len(substr) - 1
+        #     while l < r:
+        #         if substr[l] != substr[r]:
+        #             return False
+        #         l+=1
+        #         r-=1
+
+        #     return True
+
+        # palindromes = set()
+        # for l in range(len(s)):
+        #     for r in range(l+1, len(s)):
+        #         substr = s[l:r+1]
+        #         if substr in palindromes:
+        #             res[l] += 1
+        #         elif isPalin(substr) : 
+        #             palindromes.add(substr)
+        #             res[l] += 1
+        #         else:
+        #             continue
 
 
-        return sum(res)
+        # return sum(res)
