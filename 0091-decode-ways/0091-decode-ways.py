e537ca8463ad2d2a7e5bf23326b2b2a935264321
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-02 11:40:18 -0500

    Time: 0 ms (100%), Space: 19.2 MB (88.29%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,24 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if s.lstrip("0") != s: return 0 #Can't be mapped because there are leading 0s
-        res = 0
+        n = len(s)
+        if n == 0:
+            return 0
 
-        arr = [0] * (len(s)+1) # Each index will be representable by how many ways it can be decoded.
+        # dp[i+1] and dp[i+2] rolling
+        dp1 = 1 if s[-1] != '0' else 0  # dp[n-1]
+        dp2 = 1                         # dp[n] (empty string)
 
-        #Base cases
-        arr[-1] = 1
-        arr[-2] = 1 if s[-1] != "0" else 0 #zeroes cannot be represented as a standalone char, but they'll be needed for 10 & 20
-
-        for i in range(len(s)-2, -1,-1):
-            #Case when we are including only one char at i
-            si = int(s[i])
-            siPlus = int(s[i+1])
-            if si == 0: #si == 0
-                if 0 < int(s[i-1]) <= 2: #We will only be able to handle 10 and 20, if we find a 0 in the middle of the string
-                    arr[i] = 0
-                else: return 0   #Anything else will be impossible to handle
+        for i in range(n - 2, -1, -1):
+            if s[i] == '0':
+                cur = 0
             else:
-                arr[i] += arr[i+1] 
-                if 10 <= int(s[i:i+2]) <= 26:
-                    arr[i] += arr[i+2]
-        return arr[0]
+                cur = dp1  # take one digit
+                two = int(s[i:i+2])
+                if 10 <= two <= 26:
+                    cur += dp2  # take two digits
+            dp2, dp1 = dp1, cur
+
+        return dp1
