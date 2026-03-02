b96197b732ea490419728762c6d1d655b2e44f58
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-02 11:42:08 -0500

    Time: 1 ms (35.50%) | Memory: 19.1 MB (88.29%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,20 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        if n == 0:
-            return 0
+        if n == 0: return 0
 
         # dp[i+1] and dp[i+2] rolling
         dp1 = 1 if s[-1] != '0' else 0  # dp[n-1]
         dp2 = 1                         # dp[n] (empty string)
 
         for i in range(n - 2, -1, -1):
-            if s[i] == '0':
-                cur = 0
+            if s[i] == '0': cur = 0
             else:
                 cur = dp1  # take one digit
-                two = int(s[i:i+2])
-                if 10 <= two <= 26:
+                if 10 <= int(s[i:i+2]) <= 26:
                     cur += dp2  # take two digits
             dp2, dp1 = dp1, cur
 
