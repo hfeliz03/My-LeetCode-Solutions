a16396d3177000f9ef5d12197d74341069dd24ca
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-03 17:22:36 -0500

    Time: 53 ms (24.03%) | Memory: 19.3 MB (79.71%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,22 +1,25 @@
+from collections import Counter
 class Solution:
+
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1chars = {}
-        for element in s1:
-            s1chars[element] = s1chars.get(element, 0) + 1
+        s1chars = Counter(s1)
         
         l, r = 0, len(s1)
-        while r <= len(s2):
-            substr = s2[l:r]
-            s2chars = {}
-            for element in substr:
-                s2chars[element] = s2chars.get(element, 0) + 1
-            print(s2chars)
-            print(s1chars)
-            if s1chars == s2chars: 
-                
-                return True
 
+        s2chars = Counter(s2[l:r])
+        if s1chars == s2chars: return True
+
+        while r < len(s2):
+            left_char = s2[l]
+            s2chars[left_char] -= 1
+            if s2chars[left_char] == 0:
+                del s2chars[left_char]
             l+=1
+
+            right_char = s2[r]
+            s2chars[s2[r]] = s2chars.get(s2[r], 0) + 1
             r+=1
+            
+            if s1chars == s2chars: return True
 
         return False
