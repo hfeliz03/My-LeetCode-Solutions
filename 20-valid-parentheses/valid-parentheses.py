243721723d32e0cbf2b18f46ff2cec1b9045a7d3
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-05 18:08:38 -0500

    Time: 4 ms (12.34%) | Memory: 19.4 MB (26.46%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,20 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-      # (((()))))]]]
-      # If stack.peek != curr element closing parenthesis: return false 
-      # If stack is still populated at the end, return false
         if len(s) %2 != 0 : return False
+        
         PARENS = {"(": ")", "[":"]", "{":"}"}
         stack = []
         for char in s:
-
-            if char in PARENS.keys():
-                stack.append(char)
+            if char in PARENS.keys(): stack.append(char)
             else:
                 if len(stack) == 0: return False
                 lastElem = stack[-1]
-                if PARENS[lastElem] == char:
-                    stack.pop()
+                if PARENS[lastElem] == char: stack.pop()
                 else: return False
 
         return True if len(stack) == 0 else False
