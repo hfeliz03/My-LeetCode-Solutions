865d9a5f412153cdaebfd02c99a94c42079d1f6a
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-06 00:21:54 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (57.28%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,5 +1,7 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+        if n == 1: return True
+        if n % 4 != 0: return False
         for i in range(2**31 - 1):
             if 4**i == n: return True
             if 4**i > n : return False
