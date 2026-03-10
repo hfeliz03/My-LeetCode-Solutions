fa5e7013c4a0cae0f538661f90cb1ff53249217f
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-10 00:29:25 -0400

    Time: 4 ms (44.74%), Space: 21.3 MB (98.68%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,22 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # Sort 
-        # Start traversing the intervals
-        # If at some point you find conflicting meetings, start a new branch
-        # Keep the concurrent finishing times in an array
-        # return the length of that array
         if not intervals: return 0
-        intervals.sort()
-        currFinishingTimes = [intervals[0][1]]
-        for i in intervals[1:]:
-            needNewRoom = True
-            for j in range(len(currFinishingTimes)):
-                if i[0] >= currFinishingTimes[j]: 
-                    currFinishingTimes[j] = i[1] # that room can be used for the new meeting i
-                    needNewRoom = False
-                    break
 
-            if needNewRoom == True: currFinishingTimes.append(i[1]) # if the start of an interval is smaller than the end of all others on-goning, we have a conflict, need to use a new room
+        intervals.sort(key=lambda x: x[0])
 
+        min_heap = []
+        heapq.heappush(min_heap, intervals[0][1])
 
-        return len(currFinishingTimes)
+        for start, end in intervals[1:]:
+            if start >= min_heap[0]: heapq.heappop(min_heap)
+
+            heapq.heappush(min_heap, end)
+
+        return len(min_heap)
