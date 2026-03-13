c19727b17110cbb2b7d43eeb12c7496e4583c5b7
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-13 16:49:27 -0400

    Time: 138 ms (22.33%) | Memory: 31.9 MB (87.21%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -9,20 +9,20 @@ class Solution:
         #We will be doing dfs most likely because we want to check for x if X is the greater node ever seen on Xs path
         #keep the maximum at a given path, if X is smaller than its path max then X wont be good
         #Keep a global variable with the length of the tree, once you see a non-good tree, decrease it
-        def getLen(node):
-            if not node:
-                return 0
-            else: 
-                return 1 + getLen(node.left) + getLen(node.right)
+        # def getLen(node):
+        #     if not node:
+        #         return 0
+        #     else: 
+        #         return 1 + getLen(node.left) + getLen(node.right)
         
-        good = getLen(root)
+        good = 0
 
         def dfs(node, maxInPath):
             nonlocal good
             if not node: return 
             else:
-                if node.val < maxInPath:
-                    good -= 1
+                if node.val >= maxInPath:
+                    good += 1
                 maxInPath = max(node.val, maxInPath)
                 dfs(node.left, maxInPath)
                 dfs(node.right, maxInPath)
