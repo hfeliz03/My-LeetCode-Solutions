d71ada34b6a02c503160ae4671024ad39a5b0ce0
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-13 16:10:18 -0400

    Time: 434 ms (61.35%) | Memory: 21.3 MB (46.63%) - LeetSync

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minNumberOfSeconds(self, mountainHeight: int, workerTimes: List[int]) -> int:
+        h = []
+
+        for x in workerTimes:
+            heapq.heappush(h, (x,x,2)) #(Acccost of the worker, original cost, count)
+        
+        for x in range(mountainHeight):
+            acc, base, count = heapq.heappop(h)
+            res = acc
+            heapq.heappush(h, (acc + base * count, base, count + 1 ))
+        
+        return res
