681fbb067d375da6cf47c416e601f493c86e4da8
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-13 17:37:40 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (93.41%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]: 
+
+        self.i = 0
+
+        def preOrderTree(bound=float("inf")):
+            if self.i >= len(preorder) or preorder[self.i] > bound: return None
+
+            root = TreeNode(preorder[self.i])
+            self.i += 1
+            
+            root.left = preOrderTree(root.val)
+            root.right = preOrderTree(bound)
+
+            return root
+
+        
+        return preOrderTree()
