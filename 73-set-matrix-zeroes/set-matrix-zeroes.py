02e762b8099ad93bb6d29f3dda08317f070a441e
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-14 02:24:10 -0400

    Time: 11 ms (8.66%) | Memory: 20.1 MB (68.64%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -6,18 +6,44 @@ class Solution:
         #In place,
         #Track the row and cols index at where you observe the zeroes
         #then start making the changes
-        zeroRows = set()
-        zeroCols = set()
-        for i, row in enumerate(matrix):
-            for j, element in enumerate(row):
-                if element == 0: 
-                    zeroRows.add(i)
-                    zeroCols.add(j)
+        # zeros = set()
+        # for i, row in enumerate(matrix):
+        #     for j, element in enumerate(row):
+        #         if element == 0: 
+        #             zeros.add(i)
+        #             zeros.add(j)
 
-        for i, row in enumerate(matrix):
-            j = 0
-            while j < len(row):
-                if i in zeroRows or j in zeroCols:
-                    matrix[i][j] = 0
-                j+=1
+        # for i, row in enumerate(matrix):
+        #     j = 0
+        #     while j < len(row):
+        #         if i in zeros or j in zeros:
+        #             matrix[i][j] = 0
+        #         j+=1
+
+        #Forced way to do O(1) space
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0: 
+                        matrix[r][0] = 0
+                    else: 
+                        rowZero = True
+        
+        for r in range(1,ROWS):
+            for c in range(1,COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+                
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+        
+        if rowZero: 
+            for c in range(COLS):
+                matrix[0][c] = 0
+
         
