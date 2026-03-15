6079babaa87b568080a0e39e7c7e13f3c8f246af
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-14 21:47:50 -0400

    Time: 1 ms (89.47%) | Memory: 21.3 MB (18.37%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,11 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        half = len(nums)//2
+
         freqs = Counter(nums)
         maxAppearances = max(freqs.values())
-        majorityElement = 0
         for key, val in freqs.items():
             if val == maxAppearances: 
-                majorityElement = key
-                break
+                return key
 
-        return majorityElement
+        return -1
