398d51aed84722b5fa8e0f38818537d182029898
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-14 22:00:29 -0400

    Time: 40 ms (39.84%) | Memory: 45.1 MB (42.52%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -7,15 +7,26 @@
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
-        minDepth = 10**5
-        def dfs(root, level):
-            nonlocal minDepth
-            if not root.left and not root.right:
-                minDepth = min(level, minDepth)
-                return
-            print(root.val)
-            if root.left : dfs(root.left, level+1)
-            if root.right :dfs(root.right, level+1)
 
-        dfs(root, 1) 
-        return minDepth
+        if not root.left and not root.right: return 1
+
+        if not root.left:
+            return 1 + self.minDepth(root.right)
+
+        if not root.right:
+            return 1 + self.minDepth(root.left)
+
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+        # if not root: return 0
+        # minDepth = 10**5
+        # def dfs(root, level):
+        #     nonlocal minDepth
+        #     if not root.left and not root.right:
+        #         minDepth = min(level, minDepth)
+        #         return
+        #     print(root.val)
+        #     if root.left : dfs(root.left, level+1)
+        #     if root.right :dfs(root.right, level+1)
+
+        # dfs(root, 1) 
+        # return minDepth
