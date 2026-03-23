bc9c3e18aebf925bccc083224cf2d08aacb79926
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-23 13:02:30 -0400

    Time: 16 ms (5.87%) | Memory: 21.2 MB (31.03%) - LeetSync

---
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,35 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adjList = {}
+        visited = set()
+        curVisiting = set()
+        toVisit = set(element for element in range(n))
+        comps = 0
+        if len(edges) == 0: return comps
+        for x,y in edges:
+            adjList[x] = adjList.get(x, set())
+            adjList[x].add(y)
+            adjList[y] = adjList.get(y, set())
+            adjList[y].add(x)
+
+        print(toVisit)
+        print(adjList)
+
+        def bfs(neighbors):
+            while neighbors:
+                cur = neighbors.pop()
+                toVisit.discard(cur)
+                visited.add(cur)
+                for element in adjList[cur]: 
+                    if element not in visited:
+                        neighbors.add(element) 
+
+        while toVisit:
+            curNode = toVisit.pop()
+            visited.add(curNode)
+            if curNode in adjList:
+                neighbors = adjList[curNode]
+                bfs(neighbors)
+            comps += 1
+
+        return comps
