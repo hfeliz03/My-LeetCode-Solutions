37f8518c0aa1673afdac9181bf905cb74e54bec1
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-29 16:56:38 -0400

    Time: 118 ms (33.95%) | Memory: 49.1 MB (64.72%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        #I want to maximize the number of intervals, because big intervals steal space for other intervals to be there.
+        
+        intervals.sort(key = lambda x: x[1] )
+        print(intervals)
+        count = 1
+        curEnd = intervals[0][1]
+        for interval in intervals[1:]:
+            if interval[0] < curEnd:
+                continue
+            else: #Start of new interval is after/right after, the current finish time
+                curEnd = interval[1]
+                count+=1
+
+        return len(intervals) - count
