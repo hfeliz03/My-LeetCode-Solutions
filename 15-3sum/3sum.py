58d209990e1f6fede138dcadf8fdfc24285ebf77
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-03-31 11:29:15 -0400

    Time: 640 ms (54.08%) | Memory: 22.3 MB (66.47%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,16 +1,43 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        res = set()
-    
-        for i in range(len(nums)):
-            target = -nums[i] 
-            seen = set()
-            
-            for j in range(i + 1, len(nums)):
-                needed = target - nums[j]
-                if needed in seen:
-                    triplet = tuple(sorted((nums[i], nums[j], needed)))
-                    res.add(triplet)
-                seen.add(nums[j])
+        nums.sort()
+        res = []
         
-        return [list(triplet) for triplet in res]
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            l, r = i + 1, len(nums) - 1
+            
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+        
+        return res
+        # res = set()
+    
+        # for i in range(len(nums)):
+        #     target = -nums[i] 
+        #     seen = set()
+            
+        #     for j in range(i + 1, len(nums)):
+        #         needed = target - nums[j]
+        #         if needed in seen:
+        #             triplet = tuple(sorted((nums[i], nums[j], needed)))
+        #             res.add(triplet)
+        #         seen.add(nums[j])
+        
+        # return [list(triplet) for triplet in res]
