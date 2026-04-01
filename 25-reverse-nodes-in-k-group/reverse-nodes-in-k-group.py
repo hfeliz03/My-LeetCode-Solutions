2266a7c34b5841a054632d6af80b1a2f0cf8baee
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-04-01 16:39:25 -0400

    Time: 4 ms (12.69%) | Memory: 20.5 MB (31.88%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -13,29 +13,22 @@ class Solution:
         # keep track of the last thing popped because its next will be the beginning of the new reversed list
         # if eventually len(stack) < k and no more nodes, dont reverse and just return beginning of the stack
         # stack = [5] 2->1->4->3 -> 5
-
         
-        cur2 = head
-        newStart = ListNode(10**5)
-        cur = newStart
-        while cur2:
-            
+        cur = head
+        dummy = ListNode(10**5)
+        newHead = dummy
+        while cur:
             stack = []
-            while len(stack) < k and cur2: #Stack nodes till k-1
-                stack.append(cur2)
-                print(f"{stack[-1].val=}")
-                cur2 = cur2.next
-            print("stack done")
-            #print(f"{stack=}")
+            while len(stack) < k and cur: #Stack nodes till k-1
+                stack.append(cur)
+                cur = cur.next
             if len(stack) < k: #couldnt finish the entire k
-                print("hit")
-                newStart.next = stack[0]
+                dummy.next = stack[0]
             else:
-                temp = cur2.next if cur2 else None #Hold what was after k
+                temp = cur.next if cur else None #Hold what was after k
                 while stack:
-                    newStart.next = stack.pop()
-                    newStart = newStart.next
-                newStart.next = temp
-                #cur2 =  newStart.next
+                    dummy.next = stack.pop()
+                    dummy = dummy.next
+                dummy.next = temp
             
-        return cur.next
+        return newHead.next
