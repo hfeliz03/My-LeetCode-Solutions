e2eab1f211ba7d34e89509a9046e36e2cebbb7cc
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-04-27 22:15:36 -0400

    Time: 163 ms (53.42%), Space: 43.2 MB (9.59%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -1,7 +1,5 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        #All the elements must be divisible by x, otherwise impossible to make them the same
-        #Find the element in grid that's the median
         if not grid: return 0
         flatSorted = sorted([element for row in grid for element in row])
         if len(flatSorted) <= 1: return 0
