b0496aa93be1e413bcf50d2e52300538f668608a
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-05-06 23:42:56 -0400

    Time: 222 ms (69.83%) | Memory: 41 MB (29.31%) - LeetSync

---
+++ 3981-jump-game-ix/jump-game-ix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxValue(self, nums: List[int]) -> List[int]:
+        #This was very complicated, need to work on this one one more time in the future
+        n = len(nums)
+        res = [0] * n
+        s = []
+
+        for i in range(n):
+            v = nums[i]
+            l, r = i, i
+
+            while s and s[-1][0] > nums[i]:
+                lastv, lastl, lastr = s.pop()
+                v = max(v, lastv)
+                l = lastl
+            s.append((v, l, r))
+        
+        for v, l, r in s:
+            for i in range(l, r + 1):
+                res[i] = v
+        return res
