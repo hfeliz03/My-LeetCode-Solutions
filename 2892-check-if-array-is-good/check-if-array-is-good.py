205ec5317a0e18bbe7eaa4bb5678784eb5efa416
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-05-14 18:28:33 -0400

    Time: 3 ms (30.18%) | Memory: 19.3 MB (17.54%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,28 +1,13 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
         n = len(nums)
-
         seen = set()
-
         duplicate_max = False
-
         for x in nums:
-
-            if x < 1 or x > n - 1:
-
-                return False
-
+            if x < 1 or x > n - 1: return False
             if x in seen:
-
-                if x == n - 1:
-
-                    duplicate_max = True
-
-                else:
-
-                    return False
-
+                if x == n - 1: duplicate_max = True
+                else: return False
             seen.add(x)
-
         return len(seen) == n - 1 and duplicate_max
             
