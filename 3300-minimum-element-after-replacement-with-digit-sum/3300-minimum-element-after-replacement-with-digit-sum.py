a07b18856268f15aa3ef910a9d4ed21909111956
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-05-28 20:44:34 -0400

    Time: 3 ms (80.35%), Space: 19.5 MB (21.61%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -2,11 +2,9 @@ class Solution:
     def minElement(self, nums: List[int]) -> int:
         minNum = 10**5
         for num in nums:
-            numArr = list(str(num))
+            numArr = str(num)
             sumNumArr = 0
-            print(numArr)
             for numStr in numArr: 
-                numStr = int(numStr)
-                sumNumArr += numStr
+                sumNumArr += int(numStr)
             minNum = min(minNum, sumNumArr)
         return minNum
