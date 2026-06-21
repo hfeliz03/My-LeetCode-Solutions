80a08a883b9e025032a5df8608287c9f78b9e398
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-06-21 00:22:49 -0400

    Time: 83 ms (57.98%), Space: 32.1 MB (44.03%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -2,8 +2,10 @@ class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         costs.sort(reverse = True)
         res = 0
-        while coins >= 0 and costs:
-            curCost = costs.pop()
+        i = 0
+        while coins >= 0 and i < len(costs):
+            curCost = costs[-1-i]
             if coins>= curCost: res += 1
             coins -= curCost
+            i += 1
         return res
