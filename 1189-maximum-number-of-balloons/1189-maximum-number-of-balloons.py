575ca0c99357d2868b58bdbf990524e04b54f15c
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-06-21 21:26:02 -0400

    Time: 0 ms (100%), Space: 19.3 MB (75.19%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,12 +1,13 @@
+from collections import defaultdict
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        balloonSet = {"b", "a", "l", "o", "n"}
-        freqs = Counter(text)
-        for key in list(freqs.keys()):
-            if key not in balloonSet: del freqs[key]
-        print(freqs)
-        res = 10**5
         balloon = {"b":1, 'a':1, "l": 2, "o":2, "n":1}
+        freqs = defaultdict(int)
+        for key in text:
+            if key not in balloon: continue
+            freqs[key] += 1
+        res = 10**5
+        
         for key, val in balloon.items():
             if key not in freqs or val > freqs[key]: return 0
             res = min(freqs[key] // val, res)
