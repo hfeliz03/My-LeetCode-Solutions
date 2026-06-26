2d0f3bb95d6eccf75520eda3b3faf7d3a316ee73
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-06-26 08:53:51 -0400

    Time: 0 ms (100.00%) | Memory: 20.5 MB (59.38%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,28 +1,12 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        if n == 1: 
-            if nums[0] == target:
-                return [0,0]
-        elif n == 0: return [-1,-1]
-        #elif target not in set(nums): return [-1,-1]
         
         l, r = 0, n-1
 
         foundTarget = -1
-        # while l < r:
-        #     mid = (r + l) // 2
-        #     print(mid)
-        #     if nums[mid] == target:
-        #         foundTarget = mid
-        #         break
-        #     elif nums[mid] > target:
-        #         r = mid - 1
-        #     else:
-        #         l = mid + 1
         
         left = foundTarget
-        print(f'{l=}, {r=}')
         while l <= r:
             mid = (r + l) // 2
             if nums[mid] == target:
@@ -32,11 +16,9 @@ class Solution:
                 r = mid - 1
             else:
                 l = mid + 1
-        print(left)
 
         if foundTarget == -1: return [-1,-1]
-        # print(foundTarget)
-        print(f'{l=}, {r=}')
+
         l = foundTarget
         r = n-1
         right = foundTarget
@@ -48,5 +30,4 @@ class Solution:
                 l = mid + 1
             else:
                 r = mid - 1
-        print(right)
         return [left, right]
