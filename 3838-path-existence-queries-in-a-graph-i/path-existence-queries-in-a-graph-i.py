ba2197b2cb3f00eeac4194633f447a0e952c0686
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-07-08 21:10:11 -0400

    Time: 115 ms (55.55%) | Memory: 49.8 MB (63.49%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -2,15 +2,12 @@ class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
         q = len(queries)
         res = [-1] * q
-        #check in reverse all the connections
-        #for each num create an array that stores how far (the index) it can get
-        #example 2: 
-        #0,n,n,n
-        farthestI = [i for i in range(n)]
+        farthestI = [-1] * n
         for i in range(n-1,-1,-1):
             if i == n-1: continue
             farthestI[i] = i if abs(nums[i+1]-nums[i]) > maxDiff else farthestI[i+1]
-        for i in range(len(res)):
+
+        for i in range(q):
             res[i] = False if farthestI[queries[i][0]] != farthestI[queries[i][1]] else True
-        print(farthestI)
+
         return res
