74af18c99e675ddbf73162055e47ea9ff8cc70e1
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-07-13 11:21:17 -0400

    Time: 14 ms (35.16%) | Memory: 22.4 MB (16.45%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,17 +1,17 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        if not grid: return 0
-
         m, n = len(grid), len(grid[0])
+        mtx = [[-1 for _ in range(n)] for _ in range(m)]
+        mtx[-1][-1] = grid[-1][-1]
+        
+        for col in range(n-2, -1, -1):
+            mtx[-1][col] = mtx[-1][col+1] + grid[-1][col]
+        for row in range(m-2, -1, -1):
+            mtx[row][-1] = mtx[row+1][-1] + grid[row][-1]
+        
+        for i in range(m-2, -1, -1):
+            for j in range(n-2,-1,-1):
+                mtx[i][j] = grid[i][j] + min(mtx[i+1][j], mtx[i][j+1])
 
-        for i in range(1,m):
-            grid[i][0] = grid[i][0] + grid[i-1][0]
-
-        for j in range(1,n):
-            grid[0][j] = grid[0][j] + grid[0][j-1]
-
-        for i in range(1,m):
-            for j in range(1, n):
-                grid[i][j] = grid[i][j] + min(grid[i-1][j], grid[i][j-1])
-
-        return grid[-1][-1]
+        print(mtx)
+        return mtx[0][0]
