d837f5d5c945fcf672ba31a4a35e17e40a56f3db
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-07-15 13:55:05 -0400

    Time: 3080 ms (14.03%) | Memory: 100.5 MB (38.26%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,24 +1,18 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        points.sort()
-        print(points)
         res = 0
         n = len(points)
         if n <= 1: return res
 
         visited = set()
-        toVisitSet = {(x,y) for x,y in points}
-        res = 0
         frontier = [(0, (points[0][0], points[0][1]))] #curDistance, (x,y)
+        pointsSet = set([(x,y) for x,y in points])
         while frontier:
             distance, (xpoint, ypoint) = heapq.heappop(frontier)
             if (xpoint, ypoint) in visited: continue
-            for xToVisit, yToVisit in list(toVisitSet):
+            for xToVisit, yToVisit in list(pointsSet - visited):
                 heapq.heappush(frontier, ( abs(xpoint-xToVisit) + abs(ypoint - yToVisit), (xToVisit, yToVisit)) )
 
-            toVisitSet.remove((xpoint, ypoint))
             visited.add((xpoint, ypoint))
             res += distance
         return res
-
-        print(heapq.heappop(frontier))
