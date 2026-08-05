ce2389b72b2c4832394ef693436292764e57a630
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-08-05 16:06:23 -0400

    Time: 61 ms (37.63%) | Memory: 19.4 MB (96.58%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,12 +1,24 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def isPalindrome(s, didSkip):
-            if not s: return True
-            l, r = 0, len(s)-1
-            if s[l] != s[r]:
-                if didSkip: return False
-                else: 
-                    return isPalindrome(s[l:r], True) or isPalindrome(s[l+1:r+1], True)
-            else:
-                return isPalindrome(s[l+1:r], didSkip)
-        return isPalindrome(s, False)
+        def isPalindromeAfterDelete(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+
+            return True
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return (
+                    isPalindromeAfterDelete(left + 1, right)
+                    or isPalindromeAfterDelete(left, right - 1)
+                )
+
+            left += 1
+            right -= 1
+
+        return True
