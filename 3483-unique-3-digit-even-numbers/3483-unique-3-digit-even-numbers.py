da19f901ab9ede235a6bab5129c45f4cdabe6b25
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-09-10 21:23:39 -0400

    Time: 61 ms (24.62%), Space: 19.5 MB (13.85%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        setEvens = set()
+        n = len(digits)
+        def toAdd(num):
+            nonlocal setEvens
+            if len(num) == 3 and int(num) % 2 == 0:
+                print(num)
+                setEvens.add(num)
+            return
+
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(j+1, n):
+                    num1 = (str(digits[i]) + str(digits[j]) + str(digits[k])).lstrip("0")
+                    num2 = (str(digits[i]) + str(digits[k]) + str(digits[j])).lstrip("0")
+                    num3 = (str(digits[j]) + str(digits[i]) + str(digits[k])).lstrip("0")
+                    num4 = (str(digits[j]) + str(digits[k]) + str(digits[i])).lstrip("0")
+                    num5 = (str(digits[k]) + str(digits[i]) + str(digits[j])).lstrip("0")
+                    num6 = (str(digits[k]) + str(digits[j]) + str(digits[i])).lstrip("0")
+                    toAdd(num1)
+                    toAdd(num2)
+                    toAdd(num3)
+                    toAdd(num4)
+                    toAdd(num5)
+                    toAdd(num6)
+        return len(setEvens)
