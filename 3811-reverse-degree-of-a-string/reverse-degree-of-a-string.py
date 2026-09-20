82eceac88129a3a8fa9bb6aa0727217bd1f37b5c
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-09-19 21:15:46 -0400

    Time: 11 ms (20.35%) | Memory: 19.3 MB (55.57%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        res = 0
+        alphabetRev = {}
+        for i in range(26):
+            alphabetRev[chr(i+97)] = 26 - i
+        
+        print(alphabetRev)
+        for i, char in enumerate(s):
+            res += alphabetRev[char] * (i+1) 
+
+        return res
