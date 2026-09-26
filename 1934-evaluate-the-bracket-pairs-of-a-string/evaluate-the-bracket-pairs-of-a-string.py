3e2252fceb127fb87627c0be9d84378f2afbebef
Heonir Feliz <156265300+hfeliz03@users.noreply.github.com>
2026-09-25 23:47:38 -0400

    Time: 55 ms (45.17%) | Memory: 51.6 MB (47.66%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
+        knowledge = {key: val for key, val in knowledge}
+        s = s.split(")")
+        res = ""
+        for substr in s:
+            for i, char in enumerate(substr):
+                if char != "(": res += char
+                else: 
+                    key = substr[i+1:]
+                    res += knowledge[key] if key in knowledge else "?"
+                    break
+        return res
